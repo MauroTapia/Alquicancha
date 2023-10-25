e3f87f8cd3f1b4c81bf050b6728b53f78a474118
Guillermo Gardella <guillegardella@gmail.com>
2023-10-25 20:01:12 -0300

    Acá me mando la cagada

--- front/src/pages/Login/Login.jsx
+++ front/src/pages/Login/Login.jsx
@@ -1,7 +1,30 @@
+import { Label, LoginWrapper, Inputs, Remember,ImagenPerfil } from "./Login.style"
+
 
 const Login = () => {
   return (
-    <div>Login</div>
+    <>
+    <LoginWrapper>
+      
+      <ImagenPerfil>
+        <img src="/src/assets/Male User.png" alt="user" />
+      </ImagenPerfil>
+      
+      <Label>Nombre</Label>
+      <Inputs type="text" required/>
+      
+      <Label>Contraseña</Label>
+      <Inputs type="password" required/>
+      
+      <Remember>
+        <span>Remember Me </span>
+        <span>Frogot Password? </span>
+      </Remember>
+      
+      <button>Iniciar Sesión</button>
+    
+    </LoginWrapper>
+    </>
   )
 }
 
