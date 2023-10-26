7518b3efbe3178bde01731fd736f636047c69428
Guillermo Gardella <guillegardella@gmail.com>
2023-10-26 15:47:32 -0300

    se crea el Register

--- front/src/pages/Register/Register.jsx
+++ front/src/pages/Register/Register.jsx
@@ -1,7 +1,36 @@
-
+import { Label, LoginWrapper, Inputs, Remember,ImagenPerfil, Button, Button1, InputCheckBox } from "./register.style"
 const Register = () => {
   return (
-    <div>Register</div>
+    <>
+    <LoginWrapper>
+      
+      <ImagenPerfil>
+        <img src="/src/assets/Male User.png" alt="user" />
+      </ImagenPerfil>
+      
+      <Label>Name</Label>
+      <Inputs type="text" required/>
+
+      <Label>Email</Label>
+      <Inputs type="email" required/>
+      
+      <Label>Password</Label>
+      <Inputs type="password" required/>
+
+      <Label>Confirm password</Label>
+      <Inputs type="password" required/>
+      
+      <Remember>
+        <div>
+          <InputCheckBox type="checkbox"/>
+          <span>Remember me </span>
+        </div>
+      </Remember>
+      
+      <Button>Register</Button>
+    
+    </LoginWrapper>
+    </>
   )
 }
 
