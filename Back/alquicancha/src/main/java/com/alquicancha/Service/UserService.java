911457baf887a1f35213f5a1d260bcd72ef9303a
maurotapiadev <maurotapiadev@gmail.com>
2023-10-31 21:23:03 -0300

    comenzando spring 2 - MT

---
+++ Back/alquicancha/src/main/java/com/alquicancha/Service/UserService.java
@@ -0,0 +1,5 @@
+package com.alquicancha.Service;
+
+public class UserService {
+
+}
---
+++ Back/alquicancha/src/main/java/com/alquicancha/repositories/RoleRepository.java
@@ -0,0 +1,8 @@
+package com.alquicancha.repositories;
+
+import com.alquicancha.entity.Role;
+import org.springframework.data.jpa.repository.JpaRepository;
+
+public interface RoleRepository extends JpaRepository<Role, Long> {
+    Role findByName(String name);
+}
