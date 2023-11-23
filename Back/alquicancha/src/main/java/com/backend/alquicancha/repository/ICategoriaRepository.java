2c088f4e69576ae54cfafb760f848184a5f7eb9c
maurotapiadev <maurotapiadev@gmail.com>
2023-11-23 20:29:30 -0300

    arreglando back

--- Back/alquicancha/src/main/java/com/backend/alquicancha/repository/ICategoriaRepository.java
+++
@@ -1,9 +0,0 @@
-package com.backend.alquicancha.repository;
-
-import com.backend.alquicancha.entity.Categoria;
-import org.springframework.data.jpa.repository.JpaRepository;
-import org.springframework.stereotype.Repository;
-
-@Repository
-public interface ICategoriaRepository extends JpaRepository<Categoria, Long> {
-}
--- Back/alquicancha/src/main/java/com/backend/alquicancha/repository/IReservaRepository.java
+++
@@ -1,9 +0,0 @@
-package com.backend.alquicancha.repository;
-
-import com.backend.alquicancha.entity.Reserva;
-import org.springframework.data.jpa.repository.JpaRepository;
-import org.springframework.stereotype.Repository;
-
-@Repository
-public interface IReservaRepository extends JpaRepository<Reserva, Long> {
-}
