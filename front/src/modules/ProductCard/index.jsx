77faa807bea5ddeb2bd72cf4c00ef8ae3acff1e8
maurotapiadev <maurotapiadev@gmail.com>
2023-11-24 17:28:43 -0300

    ahora si funciona todo

--- front/src/modules/ProductCard/index.jsx
+++ front/src/modules/ProductCard/index.jsx
@@ -24,23 +24,31 @@ const ProductCard = ({ product }) => {
     <CardContainer>
       <Title>{title}</Title>
 
-      <Swiper navigation={true} loop modules={Navigation}>
-        {images && 
-        images.map((image, index) => (
-          <SwiperSlide key={index}>
-            <ImageWrapper>
-              <ProductImage src={image.img} alt={image.alt} />
-            </ImageWrapper>
-          </SwiperSlide>
-        ))}
-    </Swiper>
-    <Price>
-      <div>
-        <span>Precio por dia:</span> $ {dayPrice}
-      </div>
-      <ButtonDetails to={`/product/${id}`}>Ver más</ButtonDetails>
-    </Price>
-  </CardContainer>
-);
+      <Swiper
+        slidesPerView={1}
+        pagination={{
+          clickable: true,
+        }}
+        loop={true}
+        navigation={true}
+        modules={[Pagination, Navigation]}
+        >
+        {images && images.map((image, index) => (
+            <SwiperSlide key={index}>
+              <ImageWrapper>
+                <ProductImage src={image.img} alt={image.alt} />
+              </ImageWrapper>
+            </SwiperSlide>
+          ))}
+      </Swiper>
+
+      <Price>
+        <div>
+          <span>Precio por dia:</span> $ {dayPrice}
+        </div>
+        <ButtonDetails to={`/product/${id}`}>Ver más</ButtonDetails>
+      </Price>
+    </CardContainer>
+  );
 };
 export default ProductCard;
